56a51877a85ebf6effe58a93256e3fd0eb6a70da
sree <sree@buckyball>
2021-06-14 21:08:57 -0400

    Accept a list of PTX files for rtcheck

--- rtcheck.sh
+++ rtcheck.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 ptxfile"
+    echo "Usage: $0 ptxfiles..."
     exit 1;
 fi;
 
+for PF in "$@"; do
+echo "$PF"
 O1=`mktemp`
 
-if ./testparser.py "$1" -n0 -o "$O1"; then
+if ./testparser.py "$PF" -n0 -o "$O1"; then
     O2=`mktemp`
     if ./testparser.py "$O1" -n0 -o "$O2"; then
         if diff -u "$O1" "$O2"; then
@@ -15,17 +17,18 @@ if ./testparser.py "$1" -n0 -o "$O1"; then
             if ptxas -arch=sm_75 "$O2"; then
                 rm "$O2"
                 echo "OK"
-                exit 0;
+                #exit 0;
             else
                 echo "ptxas failed on '$O2'"
-                exit 1;
+                #exit 1;
             fi;
         else
             echo "$O1" "$O2"
-            exit 1;
+            #exit 1;
         fi;
     else
         echo "ERROR: Roundtrip parsing failure (stage 1 output problem)"
-        exit 1
+        #exit 1
     fi;
 fi;
+done;
